6d0d4971f78b9867d19327ad6129c1d1d229108a
p.jpapares <p.jpapares@navitaire.net>
2025-01-15 01:04:41 +0800

    Add as function to call

--- Start-Decom.ps1
+++ Start-Decom.ps1
@@ -124,93 +124,113 @@ function Sort-Resources {
     
 }
 
+function Start-Decom {
+    param (
+        
+        [Parameter(Mandatory=$true)]
+        [string]$csvFilePath,
+            
+        [Parameter(Mandatory=$true)]
+        [string]$ResourceTypeCustomSortCsvPath,
+        
+        [Parameter(Mandatory=$true)]
+        [string]$subscription,
 
-
-Write-Output "[$(Get-Date)] Starting Decommissioning process..."
-Write-Output "[$(Get-Date)] Reading worksheet.csv file..."
-Write-Output "[$(Get-Date)] Validating worksheet.csv file..."
-
-$csvContentObj = Read-WorkSheetCSV -csvFilePath .\worksheet.csv
-
-$csvSortedContentObj = Sort-Resources -csvContentObj $csvContentObj -customOrderFilePath .\ResourceTypeCustomSort.csv
-
-Write-Output $csvSortedContentObj | Format-Table -AutoSize
-
-Write-Output "[$(Get-Date)] worksheet.csv file read and validated successfully."
-Write-Output "[$(Get-Date)] Verifying worksheet content."
-
-$ResourceIdsForDecom = [System.Collections.ArrayList]::new()
-
-$csvSortedContentObj | ForEach-Object{
-
-    $resource = az resource show --ids "/subscriptions/$($_.SubscriptionId)/resourceGroups/$($_.ResourceGroup)/providers/$($_.ResourceType)/$($_.ResourceName)" | ConvertFrom-Json
+        [Parameter(Mandatory=$true)]
+        [string]$tenantId
+    )
     
-    if($resource -eq $null) {
-        Write-Error "[$(Get-Date)] Error: Resource not found. Please verify the resource details in the worksheet.csv file."
-        Write-Error "[$(Get-Date)] Error: resourceId: /subscriptions/$($_.SubscriptionId)/resourceGroups/$($_.ResourceGroup)/providers/$($_.ResourceType)/$($_.ResourceName)"
-        exit 1
-    }
-    else {
-        Write-Output "[$(Get-Date)] Checking ResourceId: $($resource.id)"
-        # Check if the resource is a web app and if it is running
-        if ($_.ResourceType -eq "Microsoft.Web/sites") {
-            Write-Output "[$(Get-Date)] Verifying Microsoft.Web/sites ..."
-            $webApp = az webapp show --resource-group $($_.ResourceGroup) --name $($_.ResourceName) | ConvertFrom-Json
-            if ($webApp.state -eq "Running") {
-                Write-Output "[$(Get-Date)] Web App $($_.ResourceName) is running."
-                Write-Output "[$(Get-Date)] Please stop the web app before running the script."
-                Write-Output "[$(Get-Date)] Exiting proccess."
-                exit 1
+    Write-Output "[$(Get-Date)] Starting Decommissioning process..."
+    Write-Output "[$(Get-Date)] Reading worksheet.csv file..."
+    Write-Output "[$(Get-Date)] Validating worksheet.csv file..."
+
+    $csvContentObj = Read-WorkSheetCSV -csvFilePath $csvFilePath
+
+    $csvSortedContentObj = Sort-Resources -csvContentObj $csvContentObj -customOrderFilePath $ResourceTypeCustomSortCsvPath
+
+    Write-Output $csvSortedContentObj | Format-Table -AutoSize
+
+    Write-Output "[$(Get-Date)] worksheet.csv file read and validated successfully."
+    Write-Output "[$(Get-Date)] Verifying worksheet content."
+
+    $ResourceIdsForDecom = [System.Collections.ArrayList]::new()
+
+    $csvSortedContentObj | ForEach-Object{
+
+        $resource = az resource show --ids "/subscriptions/$($_.SubscriptionId)/resourceGroups/$($_.ResourceGroup)/providers/$($_.ResourceType)/$($_.ResourceName)" | ConvertFrom-Json
+        
+        if($resource -eq $null) {
+            Write-Error "[$(Get-Date)] Error: Resource not found. Please verify the resource details in the worksheet.csv file."
+            Write-Error "[$(Get-Date)] Error: resourceId: /subscriptions/$($_.SubscriptionId)/resourceGroups/$($_.ResourceGroup)/providers/$($_.ResourceType)/$($_.ResourceName)"
+            exit 1
+        }
+        else {
+            Write-Output "[$(Get-Date)] Checking ResourceId: $($resource.id)"
+            # Check if the resource is a web app and if it is running
+            if ($_.ResourceType -eq "Microsoft.Web/sites") {
+                Write-Output "[$(Get-Date)] Verifying Microsoft.Web/sites ..."
+                $webApp = az webapp show --resource-group $($_.ResourceGroup) --name $($_.ResourceName) | ConvertFrom-Json
+                if ($webApp.state -eq "Running") {
+                    Write-Output "[$(Get-Date)] Web App $($_.ResourceName) is running."
+                    Write-Output "[$(Get-Date)] Please stop the web app before running the script."
+                    Write-Output "[$(Get-Date)] Exiting proccess."
+                    exit 1
+                }
             }
+
+            # Check if the resource is an App Service Plan and if it still has web apps
+            if ($_.ResourceType -eq "Microsoft.Web/serverfarms") {
+                Write-Output "[$(Get-Date)] Verifying Microsoft.Web/serverfarms ..."
+                $webAppsInAsp = az webapp list --query "[?appServicePlanId=='$($resource.id)']" | ConvertFrom-Json
+                # If the web app in ASP count is greater than 1, then the ASP still shared
+                if($webAppsInAsp.Count -gt 1) { 
+                    Write-Output "[$(Get-Date)] App Service Plan $($_.ResourceName) still has web apps $($webAppsInAsp.name)."
+                    Write-Output "[$(Get-Date)] Please move the web apps to another App Service Plan before running the script."
+                    Write-Output "[$(Get-Date)] Exiting proccess."
+                    exit 1
+                }
+                # Else the web app in ASP count is 1, then the ASP is not shared and can be decommissioned
+            }
+
+            $ResourceIdsForDecom.Add($resource) | Out-Null
+        }
+    }
+
+    $decommissionedResources = [System.Collections.ArrayList]::new()
+    foreach($resource in $ResourceIdsForDecom){
+        $DecomStatus = ""
+        $DecomRemarks = ""
+
+        Write-Output "[$(Get-Date)] Decommissioning $($resource.name) ..."
+        az resource delete --ids $resource.id
+        if ($LASTEXITCODE -ne 0) {
+            Write-Error "[$(Get-Date)] Error: Failed to decommission resource: $($resource.name) with ResourceId: $($resource.id)."
+            $DecomStatus = "Failed"
+            $DecomRemarks = "Failed to decommission"
+        }
+        else {
+            $DecomStatus = "Deleted"
+            $DecomRemarks = "Successfully decommissioned"
         }
 
-        # Check if the resource is an App Service Plan and if it still has web apps
-        if ($_.ResourceType -eq "Microsoft.Web/serverfarms") {
-            Write-Output "[$(Get-Date)] Verifying Microsoft.Web/serverfarms ..."
-            $webAppsInAsp = az webapp list --query "[?appServicePlanId=='$($resource.id)']" | ConvertFrom-Json
-            if($webAppsInAsp.Count -gt 1) {
-                Write-Output "[$(Get-Date)] App Service Plan $($_.ResourceName) still has web apps $($webAppsInAsp.name)."
-                Write-Output "[$(Get-Date)] Please move the web apps to another App Service Plan before running the script."
-                Write-Output "[$(Get-Date)] Exiting proccess."
-                exit 1
-            }
+        $decommissionedResource = [PSCustomObject]@{
+            ResourceName = $resource.name
+            ResourceType = $resource.type
+            ResourceLocation = $resource.location
+            ResourceId = $resource.id
+            DecomStatus = $DecomStatus
+            Remarks = $DecomRemarks
+            Date = Get-Date
         }
 
-        $ResourceIdsForDecom.Add($resource) | Out-Null
-    }
-}
+        $decommissionedResources.Add($decommissionedResource)
 
-$decommissionedResources = [System.Collections.ArrayList]::new()
-foreach($resource in $ResourceIdsForDecom){
-    $DecomStatus = ""
-    $DecomRemarks = ""
-
-    Write-Output "[$(Get-Date)] Decommissioning $($resource.name) ..."
-    az resource delete --ids $resource.id
-    if ($LASTEXITCODE -ne 0) {
-        Write-Error "[$(Get-Date)] Error: Failed to decommission resource: $($resource.name) with ResourceId: $($resource.id)."
-        $DecomStatus = "Failed"
-        $DecomRemarks = "Failed to decommission"
-    }
-    else {
-        $DecomStatus = "Deleted"
-        $DecomRemarks = "Successfully decommissioned"
     }
 
-    $decommissionedResource = [PSCustomObject]@{
-        ResourceName = $resource.name
-        ResourceType = $resource.type
-        ResourceLocation = $resource.location
-        ResourceId = $resource.id
-        DecomStatus = $DecomStatus
-        Remarks = $DecomRemarks
-        Date = Get-Date
-    }
+    $decommissionedResources | Export-Csv -Path ".\$(Get-Date -Format "yyyyMMddHHmmss")_DecommissionedResources.csv" -NoTypeInformation
 
-    $decommissionedResources.Add($decommissionedResource)
+    Write-Output "[$(Get-Date)] Decommissioning process completed successfully."
 
 }
 
-$decommissionedResources | Export-Csv -Path ".\$(Get-Date -Format "yyyyMMddHHmmss")_DecommissionedResources.csv" -NoTypeInformation
-
-Write-Output "[$(Get-Date)] Decommissioning process completed successfully."
+Start-Decom -csvFilePath .\worksheet.csv -ResourceTypeCustomSortCsvPath .\ResourceTypeCustomSort.csv -subscription "2d1c7006-7070-45e4-a17a-c791cad26905" -tenantId "b3f4f7c2-72ce-4192-aba4-d6c7719b5766"
